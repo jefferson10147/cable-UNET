2e25c00b5714a2f539665b186e82af490b4dfa82
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-03-11 18:54:59 -0400

    Setting phone services model

---
+++ app/Models/PhoneService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PhoneService extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'limit',
+        'price',
+    ];
+
+    public $timestamps = false;
+}

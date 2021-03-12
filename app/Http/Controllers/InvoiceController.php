b82bd793f38e5fb01ba625d7a16830d21c86cd74
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-03-12 01:20:14 -0400

    Setting Invoice controller

---
+++ app/Http/Controllers/InvoiceController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Invoice;
+
+class InvoiceController extends Controller
+{
+    public function saveInvoice(Request $request)
+    {
+        $invoice = Invoice::create([
+            'user_id' => 1,
+            'amount' => 29.99
+        ]);
+
+        return view('welcome');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\InternetServiceController;
 use App\Http\Controllers\PhoneServiceController;
 use App\Http\Controllers\CableServiceController;
 use App\Http\Controllers\ChannelController;
+use App\Http\Controllers\InvoiceController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,3 +15,4 @@ Route::get('/save/internet_service', [InternetServiceController::class, 'saveInt
 Route::get('/save/phone_service', [PhoneServiceController::class, 'savePhoneService'])->name('phoneService.save');
 Route::get('/save/cable_service', [CableServiceController::class, 'saveCableService'])->name('cableService.save');
 Route::get('/save/channel', [ChannelController::class, 'saveChannel'])->name('channel.save');
+Route::get('/save/invoice', [InvoiceController::class, 'saveInvoice'])->name('invoice.save');

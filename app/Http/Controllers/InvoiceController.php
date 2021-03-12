ff9f6f32a99ba25876a1ed5c274d4005ee4af05b
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-03-12 01:53:42 -0400

    Commenting some code at controllers

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -9,11 +9,12 @@ class InvoiceController extends Controller
 {
     public function saveInvoice(Request $request)
     {
+        /*
         $invoice = Invoice::create([
             'user_id' => 1,
             'amount' => 29.99
         ]);
-
+        */
         return view('welcome');
     }
 }
--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -9,11 +9,13 @@ class PackageController extends Controller
 {
     public function savePackage(Request $request)
     {
+        /*
         $package = Package::create([
             'cable_service_id' => 1,
             'channel_id' => 1
         ]);
-
+        */
+        
         return view('welcome');
     }
 }

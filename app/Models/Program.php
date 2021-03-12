4dd9ad30eabb80bca02fe842429f6bdc5e81f4f8
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-03-12 01:55:45 -0400

    Setting programs model

---
+++ app/Models/Program.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Program extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'channel_id',
+        'date',
+        'program_name',
+        'starts_at',
+        'ends_at'
+    ];
+
+    public $timestamps = false;
+}

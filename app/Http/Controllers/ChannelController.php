fe1104f8c8f76d347a98cc4d09ab07752ad16d87
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-03-12 00:50:32 -0400

    Making invoices model table

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -9,12 +9,13 @@ class ChannelController extends Controller
 {
     public function saveChannel(Request $request)
     {
+        /*
         $channel = Channel::create([
             'name' => 'FOX Sports',
             'description' => 'Your favorite sports channel',
             'price' => 1.99
         ]);
-
+        */
         return view('welcome');
     }
 }

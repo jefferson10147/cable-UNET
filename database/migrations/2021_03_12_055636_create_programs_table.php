4ee24e326acf85f478034fa0d1337a0c59fffd8e
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-03-12 01:59:07 -0400

    Making Programs table migration

---
+++ database/migrations/2021_03_12_055636_create_programs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProgramsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('programs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('channel_id');
+            $table->foreign('channel_id')->references('id')->on('channels')->onDelete('cascade')->onDelete('cascade');
+            $table->date('date');
+            $table->string('program_name', 30);
+            $table->time('starts_at');
+            $table->time('ends_at');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('programs');
+    }
+}

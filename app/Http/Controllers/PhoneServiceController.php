ad20bf4044774964121abe5b279368e8cfd43e4a
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-03-12 23:57:48 -0400

    Making store validations at phone services controller

--- app/Http/Controllers/PhoneServiceController.php
+++ app/Http/Controllers/PhoneServiceController.php
@@ -7,15 +7,89 @@ use App\Models\PhoneService;
 
 class PhoneServiceController extends Controller
 {
-    public function savePhoneService(Request $request)
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        /*
-        $phoneService = PhoneService::create([
-            'name' => 'simple',
-            'plan_limit' => '30 min',
-            'price' => 3.99 
+        $phoneServices = PhoneService::all();
+        dd($phoneServices);
+        
+        return view('phone_services_list', compact('phoneServices'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('create_phone_service');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string'],
+            'plan_limit' => ['required', 'string'],
+            'price' => ['required', 'numeric']
         ]);
-        */
-        return view('welcome');
+        PhoneService::create($request->all());
+        
+        return redirect('phone_services/create')->with('succes', 'The phone service have been correctly registered');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }

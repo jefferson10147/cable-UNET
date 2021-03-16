0d0ad3d410b21f454889c815070beb134e6300d1
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-03-16 02:30:04 -0400

    Adding navbar into schedules view

--- resources/views/show_schedules.blade.php
+++ resources/views/show_schedules.blade.php
@@ -10,6 +10,23 @@
     <title>Schedules</title>
 </head>
 <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <a class="navbar-brand" href="{{ route('home') }}">Cable Unet</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto"></ul>
+          <form class="form-inline my-2 my-lg-0" method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Logout</button>
+          </form>
+        </div>
+    </nav>
+
+
     @foreach ($channels as $channel)
         @if (!$channel->programs->isEmpty())
             <div class="container">
